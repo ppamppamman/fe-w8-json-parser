9889d3f36dea4385e9de6657501165a82a0d05ff
Jaeuk-YOO <ryou9305@naver.com>
2021-03-30 17:26:57 +0900

    fix: 필요없는 임시 테스트파일 삭제

--- src/test/test03.js
+++
@@ -1,8 +0,0 @@
-const data = `{ 
-    "key1": 1,
-    "key2": [1, 2]
-  }`;
-const data1 = `["la3", [null, false, ["11", [112233], { easy: ["hello", { a: "a" }, "world"] }, 112], 55, "99"], 
-  { a: "str", b: [912, [5656, 33], { key: "innervalue", newkeys: [1, 2, 3, 4, 5] }] }, true]`;
-const data2 = `{"easy" : ["hello", {"a":"a"}, "world"], "hard":"hi"}`;
-const testQuote = `[a,"b",[c],"d[a]"]`;
