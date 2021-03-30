e6be1d99fde78df9837799302258182715459b97
Jaeuk-YOO <ryou9305@naver.com>
2021-03-30 18:11:48 +0900

    add: 초기 데이터 기준 확인작업

--- src/index.js
+++ src/index.js
@@ -2,24 +2,42 @@ const tokenize = require('./util/tokenizer.js');
 const lexicalize = require('./util/lexer.js');
 const generate = require('./util/treeGenerator.js');
 
-const data1 = `["la3", [null, false, ["11", [112233], { easy: ["hello", { a: "a" }, "world"] }, 112], 55, "99"], 
-  { a: "str", b: [912, [5656, 33], { key: "innervalue", newkeys: [1, 2, 3, 4, 5] }] }, true]`;
-const splitData = data1.replace(/(\r\n\t|\n|\r\t)|\s/g, "");
+const data = `
+[
+  "la3"
+  ,
+   [null, false, ["11", [112233], { easy: ["hello", { a: "a" }, "world"] }, 112], 55, "99"]
+   , 
+  { a: "str", b: [912, [5656, 33], { key: "innervalue", newkeys: [1, 2, 3, 4, 5] }] }
+  ,
+  true  
+]
+`;
+const splitData = data.replace(/(\r\n\t|\n|\r\t)|\s/g, "");
 
 const tokenArray = tokenize(splitData);
 const lexicalizedTokens = lexicalize(tokenArray);
 
-const tokens = [
-  '{',       '"easy"',  ':',
-  '[',       '"hello"', ',',
-  '{',       '"a"',     ':',
-  '"a"',     '}',       ',',
-  '"world"', ']',       ',',
-  '"hard"',  ':',       '"hi"',
-  '}'
-]
+const lexicalizedTree = generate(lexicalizedTokens);
 
-const parsedTree = generate(tokens);
+const parsing = (me) => {
+  
+  if (me.constructor.name === "Branch") {
+    if (me.getLeafNodes().length !== 0) {
+      me.getLeafNodes().forEach((node) => {
+        parsing(node)
+      })
+    }
+  } else {
+    console.log(me);
+  }
+}
+parsing(lexicalizedTree.getRoot());
 
-console.log(lexicalizedTokens)
-
+// const util = require('util');
+// console.log(util.inspect(parsedTree, false, null, true));
+// console.log(lexerTree.getRoot())
+// console.log(parsedTree.getRoot().getLeafNodes().length)
+// lexerTree.getRoot().getLeafNodes().forEach((node) => {
+//   // console.log(node)
+// })
