863a5e8d10d3098f30143ade8e99c7a01c9b17b8
Jungwon Lee <leejungwon96@gmail.com>
2021-04-01 17:10:48 +0900

    Fix. 분석 버튼 클릭시 데이터 잘 넘어가도록 구조 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,39 @@ import "./app.scss";
 class App {
   constructor({ $target }) {
     this.$target = $target;
+
+    // 컴포넌트 영역
+    this.JsonInputContainer = null;
+    this.JsonOutputContainer = null;
+
     this.setState({ $target });
   }
+
+  init() {}
+
   setState({ $target }) {
     this.render({ $target });
   }
+
   render({ $target }) {
     const $arrowSection =
       /*html*/
       `<section class="arrow__section">👉</section>`;
 
-    new JsonInputContainer({ $target });
+    this.JsonInputContainer = new JsonInputContainer({
+      $target,
+      onParseBunttonClick: this.handleParseBunttonClick.bind(this),
+    });
+
     $target.insertAdjacentHTML("beforeend", $arrowSection);
     //beforeend => element 안에 가장 마지막 child. 약간 append 하는걸까?
-    new JsonOutputContainer({ $target });
+    this.JsonOutputContainer = new JsonOutputContainer({ $target });
+  }
+
+  handleParseBunttonClick(value) {
+    console.log("app handle", value); // => json으로 넘겨줘야 하는 .
+    const nextState = { type: "JSON_INPUT", value: value };
+    this.JsonOutputContainer.setState(nextState);
   }
 }
 
--- frontend/src/Components/JsonOutput/JsonOutputContainer.js
+++ frontend/src/Components/JsonOutput/JsonOutputContainer.js
@@ -2,38 +2,63 @@ import JsonOutputPresentational from "./JsonOutputPresentational.js";
 
 class JsonOutputContainer {
   constructor({ $target }) {
-    //
+    // 돔
     this.$target = $target;
 
-    this.state;
-    this.setState({ $target });
-    const $parseBtn = document.querySelector(".json-data__button");
-    const $jsonInput = document.querySelector(".json-data__input");
-    $parseBtn.addEventListener("click", () => {
-      console.log($jsonInput.value);
-      //여기에서의 input값을 POST할때 body에 넣어야할것같습니다.
-      this.postString();
+    // 컴포넌트
+    this.JsonOutputPresentational = null;
+
+    // 상태
+    this.state = {
+      jsonInput: null, // 유저가 입력한 json처럼 생긴 스트링 => 버튼 클릭시 넘어옴
+      parsedJsonInput: null, // this.state.jsonInput => 파서를 통해서 파싱하고 return 된 결과 값
+    };
+
+    this.setState({});
+
+    // 레거시
+    // const $parseBtn = document.querySelector(".json-data__button");
+    // const $jsonInput = document.querySelector(".json-data__input");
+    // $parseBtn.addEventListener("click", () => {
+    //   console.log($jsonInput.value);
+    //   //여기에서의 input값을 POST할때 body에 넣어야할것같습니다.
+    //   this.postString();
+    // });
+  }
+
+  setState(nextState) {
+    console.log(nextState);
+    if (nextState.type === "JSON_INPUT") {
+      this.state.jsonInput = nextState.value;
+      this.onParseButtonClicked(); // 보낸다.
+    } else if (nextState.type === "PARSED_JSON_INPUT") {
+      this.state.parsedJsonInput = nextState.value;
+    }
+
+    //여기서 target, 파싱된 데이터를 받아서 presentational에 렌더시 보내줄듯
+    this.render();
+  }
+
+  render() {
+    // 렌더가 쫘라락 된다.
+    this.JsonOutputPresentational = new JsonOutputPresentational({
+      $target: this.$target,
+      parsedJsonInput: this.state.parsedJsonInput,
     });
   }
-  init() {
+
+  onParseButtonClicked() {
     // output
-    this.postString({}).then((res) => {
+    this.postString(this.state.jsonInput).then((res) => {
       console.log(res);
+
       this.setState({ nextState: res });
       // input에서 넘김=> 버튼누르면 input value가 post body로 넘어감 => 서버에서 파싱작업 해줌=> response오면 seState를 실행시켜서 리렌더링함
     });
   }
-  setState({ $target, nextState }) {
-    this.state = nextState;
-    //여기서 target, 파싱된 데이터를 받아서 presentational에 렌더시 보내줄듯
-    this.render({ $target });
-  }
-  render({ $target }) {
-    // 렌더가 쫘라락 된다.
-    new JsonOutputPresentational({ $target });
-  }
 
   async postString(body) {
+    console.log(body);
     const response = await fetch("http://localhost:3030/parser", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
