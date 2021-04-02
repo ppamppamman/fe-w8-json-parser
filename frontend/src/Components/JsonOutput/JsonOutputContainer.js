d2d9831fef9d45231f683b8e56791608afa22702
Jaeuk-YOO <ryou9305@naver.com>
2021-04-02 14:05:59 +0900

    add: backend 연동 진행 코드

--- frontend/src/Components/JsonOutput/JsonOutputContainer.js
+++ frontend/src/Components/JsonOutput/JsonOutputContainer.js
@@ -4,7 +4,6 @@ import "./jsonOutput.scss";
 class JsonOutputContainer {
   constructor({ $target }) {
     // 돔
-    // this.$target = $target;
     const $JsonOutputArea = document.createElement("div");
     this.$target = $JsonOutputArea;
     $target.append(this.$target);
@@ -16,10 +15,8 @@ class JsonOutputContainer {
     this.state = {
       jsonInput: null, // 유저가 입력한 json처럼 생긴 스트링 => 버튼 클릭시 넘어옴
       parsedJsonInput: null, // this.state.jsonInput => 파서를 통해서 파싱하고 return 된 결과 값
-      // isFirstRender: true,
     };
 
-    // this.setState({ isFirstRender: this.state.isFirstRender });
     this.setState({});
   }
 
@@ -28,7 +25,7 @@ class JsonOutputContainer {
     if (nextState.type === "JSON_INPUT") {
       this.state.jsonInput = nextState.value;
       this.onParseButtonClicked(); // 보낸다.
-      // this.state.isFirstRender = false;
+      
     } else if (nextState.type === "PARSED_JSON_INPUT") {
       this.state.parsedJsonInput = nextState.value;
     }
@@ -38,16 +35,15 @@ class JsonOutputContainer {
   }
 
   render() {
-    // 렌더가 쫘라락 된다.
     this.JsonOutputPresentational = new JsonOutputPresentational({
       $target: this.$target,
       parsedJsonInput: this.state.parsedJsonInput,
-      // isFirstRender: this.state.isFirstRender,
     });
   }
 
   onParseButtonClicked() {
     // output
+    console.log("onParseButtonClicked", this.state.jsonInput)
     this.postString(this.state.jsonInput).then((res) => {
       console.log(res);
       this.setState({ nextState: res });
@@ -55,12 +51,10 @@ class JsonOutputContainer {
   }
 
   async postString(body) {
-    console.log(body);
-    const response = await fetch("http://localhost:3333/data", {
+    const response = await fetch("http://localhost:3333/data/", {
       method: "POST",
-      headers: { Accept: "application/json" },
-      body: body,
-      // body: JSON.stringify(body),
+      headers: { Accept: "application/json", 'Content-Type': 'application/json' },
+      body: JSON.stringify({ body }),
     });
     return response.json();
   }
