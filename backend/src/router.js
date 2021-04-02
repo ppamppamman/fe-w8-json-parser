0d3c5987afa498a0ada22718847d47ceef8429c1
Jaeuk-YOO <ryou9305@naver.com>
2021-04-02 15:44:18 +0900

    fix: 백엔드 코드 리팩토링

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,23 +1,15 @@
 const express = require("express");
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 
 const router = express.Router();
 
 const state = { jsonInput: null };
 
 router.get("/", (req, res) => {
-  console.log(state);
   return res.status(200).send({ data: state.jsonInput });
 });
 
 router.post("/", async (req, res) => {
-  // const result = await fetch(recommendKeywordsEndPoint(req.body.query)).then(
-  //   (response) => {
-  //     return response.text();
-  //   }
-  // )
-  console.log(req.body);
-  // console.log(req.body)
   state.jsonInput = req.body.data;
   return res.status(200).send({ response: req.body });
 });
