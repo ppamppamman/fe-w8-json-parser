e46bfdfdace207891fce1410af224b62bdc5adb9
Jungwon Lee <leejungwon96@gmail.com>
2021-04-02 12:22:20 +0900

    test. body값 안불러와짐... 테스트중

--- frontend/src/Components/JsonInput/JsonInputContainer.js
+++ frontend/src/Components/JsonInput/JsonInputContainer.js
@@ -5,13 +5,15 @@ class JsonInputContainer {
   constructor({ $target, onParseBunttonClick }) {
     this.JsonInputPresentational = null;
     this.onParseBunttonClick = onParseBunttonClick;
-
-    this.init({ $target });
+    const $JsonInputArea = document.createElement("div");
+    this.$target = $JsonInputArea;
+    $target.append(this.$target);
+    this.render({ $target: this.$target });
   }
 
-  init({ $target }) {
-    this.render({ $target });
-  }
+  // init({ $target }) {
+  //   this.render({ $target });
+  // }
 
   // 만약 모델이 있었으면, 모델을 옵저버하는게 베스트
 
